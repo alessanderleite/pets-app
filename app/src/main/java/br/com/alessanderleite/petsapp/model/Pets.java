985669de0dbd963ccbf7573ac2303ebfb5ce0428
alessanderleite <alessander.logistica@gmail.com>
2019-06-11 13:37:17 -0300

    refactor the Pets.java

--- app/src/main/java/br/com/alessanderleite/petsapp/model/Pets.java
+++ app/src/main/java/br/com/alessanderleite/petsapp/model/Pets.java
@@ -1,4 +1,4 @@
-package br.com.alessanderleite.petsapp;
+package br.com.alessanderleite.petsapp.model;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
