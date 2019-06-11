0df92b5a8203e7082fabcfbb1a884849100fb252
alessanderleite <alessander.logistica@gmail.com>
2019-06-11 18:08:05 -0300

    update the adapter code

--- app/src/main/java/br/com/alessanderleite/petsapp/Adapter.java
+++ app/src/main/java/br/com/alessanderleite/petsapp/Adapter.java
@@ -8,27 +8,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.alessanderleite.petsapp.model.Pets;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> implements Filterable {
 
-    List<Pets> pets;
+    List<Pets> pets, petsFilter;
     private Context context;
+    private RecyclerViewClickListener mListener;
+    CustomFilter filter;
 
-    public Adapter(List<Pets> pets, Context context) {
+    public Adapter(List<Pets> pets, Context context, RecyclerViewClickListener listener) {
         this.pets = pets;
+        this.petsFilter = pets;
         this.context = context;
+        this.mListener = listener;
     }
 
     @NonNull
     @Override
     public Adapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-        return new MyViewHolder(view);
+        return new MyViewHolder(view, mListener);
     }
 
     @Override
@@ -45,18 +52,42 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> implemen
 
     @Override
     public Filter getFilter() {
-        return null;
+        if (filter == null) {
+            filter = new CustomFilter((ArrayList<Pets>) petsFilter, this);
+        }
+        return filter;
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        private RecyclerViewClickListener mListener;
         private TextView mName, mType;
+        private RelativeLayout mRowContainer;
 
-        public MyViewHolder(@NonNull View itemView) {
+        public MyViewHolder(@NonNull View itemView, RecyclerViewClickListener listener) {
             super(itemView);
 
             mName = (TextView) itemView.findViewById(R.id.name);
             mType = (TextView) itemView.findViewById(R.id.type);
+            mRowContainer = (RelativeLayout) itemView.findViewById(R.id.row_container);
+
+            mListener = listener;
+            mRowContainer.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.row_container:
+                    mListener.onRowClick(mRowContainer, getAdapterPosition());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public interface RecyclerViewClickListener {
+        void onRowClick(View view, int position);
     }
 }
