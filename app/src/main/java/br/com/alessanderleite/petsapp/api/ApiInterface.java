ad6a4be6710fd927e945cb4f00f521accaa80709
alessanderleite <alessander.logistica@gmail.com>
2019-06-11 13:34:50 -0300

    build the ApiInterface

---
+++ app/src/main/java/br/com/alessanderleite/petsapp/api/ApiInterface.java
@@ -0,0 +1,14 @@
+package br.com.alessanderleite.petsapp.api;
+
+import java.util.List;
+
+import br.com.alessanderleite.petsapp.Pets;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiInterface {
+
+    @GET("/pets")
+    Call<List<Pets>> getPets();
+
+}
