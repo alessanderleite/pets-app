cdb0616ef08735f81ae6c1674acc1d35e0c1bfd9
alessanderleite <alessander.logistica@gmail.com>
2019-06-11 13:24:59 -0300

    create the model class named Pets

---
+++ app/src/main/java/br/com/alessanderleite/petsapp/Pets.java
@@ -0,0 +1,44 @@
+package br.com.alessanderleite.petsapp;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Pets {
+
+    @SerializedName("petId")
+    @Expose
+    private Integer petId;
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("species")
+    @Expose
+    private String species;
+
+    public Integer getPetId() {
+        return petId;
+    }
+
+    public void setPetId(Integer petId) {
+        this.petId = petId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+}
