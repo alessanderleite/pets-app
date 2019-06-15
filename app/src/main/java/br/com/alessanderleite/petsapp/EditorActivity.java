5117ed9a9fde458856ec01c236b5179885709144
alessanderleite <alessander.logistica@gmail.com>
2019-06-14 23:03:47 -0300

    add the postData() method

--- app/src/main/java/br/com/alessanderleite/petsapp/EditorActivity.java
+++ app/src/main/java/br/com/alessanderleite/petsapp/EditorActivity.java
@@ -1,21 +1,27 @@
 package br.com.alessanderleite.petsapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.EditText;
 
+import br.com.alessanderleite.petsapp.api.ApiClient;
 import br.com.alessanderleite.petsapp.api.ApiInterface;
+import br.com.alessanderleite.petsapp.model.Pet;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class EditorActivity extends AppCompatActivity {
 
-    private EditText mName, mSpecies;
+    private EditText mName, mSpecies, mBreed;
 
-    private String name, species;
+    private String name, species, breed;
     private int id;
 
     private Menu action;
@@ -71,5 +77,50 @@ public class EditorActivity extends AppCompatActivity {
             action.findItem(R.id.menu_save).setVisible(true);
         }
         return true;
+    }
+
+    private void postData(final String key) {
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Saving...");
+        progressDialog.show();
+
+        readMode();
+
+        String name = mName.getText().toString().trim();
+        String species = mSpecies.getText().toString().trim();
+        String breed = mBreed.getText().toString().trim();
+
+        apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        Call<Pet> call = apiInterface.insertPet(key, name, species, breed);
+
+        call.enqueue(new Callback<Pet>() {
+            @Override
+            public void onResponse(Call<Pet> call, Response<Pet> response) {
+                progressDialog.dismiss();
+
+                Log.i(EditorActivity.class.getSimpleName(), response.body());
+
+                response.body();
+            }
+
+            @Override
+            public void onFailure(Call<Pet> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    void readMode() {
+
+        mName.setFocusableInTouchMode(false);
+        mSpecies.setFocusableInTouchMode(false);
+        mBreed.setFocusableInTouchMode(false);
+
+        mName.setFocusable(false);
+        mSpecies.setFocusable(false);
+        mBreed.setFocusable(false);
     }
 }
