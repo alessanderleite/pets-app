5e1b8f36343a3cc33e460e07b3c7700592be7d1d
alessanderleite <alessander.logistica@gmail.com>
2019-06-14 21:59:24 -0300

    add the @POST annotation

--- app/src/main/java/br/com/alessanderleite/petsapp/api/ApiInterface.java
+++ app/src/main/java/br/com/alessanderleite/petsapp/api/ApiInterface.java
@@ -4,11 +4,21 @@ import java.util.List;
 
 import br.com.alessanderleite.petsapp.model.Pet;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiInterface {
 
     @GET("/pets")
     Call<List<Pet>> getPets();
 
+    @POST("/create")
+    @FormUrlEncoded
+    Call<Pet> insertPet(
+            @Field("name") String name,
+            @Field("species") String species,
+            @Field("breed") String breed
+    );
 }
